6c0b528a270b038b3b6c0d196a6c31374982932b
Ram <itsram7@gmail.com>
2020-09-20 19:02:18 +0530

    server file changes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/spaceX-app/'));
+app.use(express.static('./dist/spaceX-app/browser'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/spaceX-app/'}),
+    res.sendFile('index.html', {root: 'dist/spaceX-app/browser/'}),
 );
 
 app.listen(process.env.PORT || 8080);
